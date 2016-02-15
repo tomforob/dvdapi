69dc398155aa629b74e3356a99e938ebdac1b654
tomforob <tom@forob.com>
2016-02-15 14:25:01 -0700

    test

--- app.js
+++ app.js
@@ -132,6 +132,7 @@ app.use('/api/category', categoryRouter);
 app.get('/', function(req, res) {
 //  res.send('welcome to my api');
   res.send(msg + numcpus + ' ' + process.env.WEB_MEMORY + util.inspect(process.memoryUsage());
+  console.log(util.inspect(process.memory_Usage()));
   });
 
 app.listen(port, function() {
